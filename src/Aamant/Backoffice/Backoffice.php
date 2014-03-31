0d6fb63267df8cb074bcaa7493306f0505bad0d2
Arnaud Amant <arnaud.amant@gmail.com>
2014-03-31 14:50:17 +0200

    Add support to aamant/menu 1.1.0

--- src/Aamant/Backoffice/Backoffice.php
+++ src/Aamant/Backoffice/Backoffice.php
@@ -10,14 +10,26 @@ class Backoffice
 	{
 		$this->menu = $menu;
 
-		$this->menu->addItem('dashbord', array('url' => $this->baseurl(), 'label' => 'Dashbord', 'rank' => 1));
-		$this->menu->addItem('settings', array('url' => $this->baseurl() . '/settings/general', 'label' => 'Settings', 'rank' => 10));
-		$this->menu->addItem('profile', array('url' => $this->baseurl() . '/profile', 'label' => 'Profile', 'rank' => 20));
-		$this->menu->addItem('site', array('url' => '/', 'label' => 'Voir le site', 'target' => '_blank'));
+		$this->menu->addItem('dashbord', 
+			\App::make('menu-item', array('url' => $this->baseurl(), 'label' => 'Dashbord', 'rank' => 1)));
+		$this->menu->addItem('settings', 
+			\App::make('menu-item', array('url' => $this->baseurl() . '/settings/general', 'label' => 'Settings', 'rank' => 10)));
+
+		$submenu = \App::make('menu', array('label' => sprintf('%s %s', \Auth::user()->firstname, \Auth::user()->lastname), 'rank' => 1000));
+
+		$submenu->addItem('profile', 
+			\App::make('menu-item', array('url' => $this->baseurl() . '/profile', 'label' => 'Profile', 'rank' => 10)));
+		$submenu->addItem('logout', 
+			\App::make('menu-item', array('url' => '/auth/logout', 'label' => 'Logout', 'rank' => 1000)));
+		$this->menu->addItem('user', $submenu);
+
+		$this->menu->addItem('site', 
+			\App::make('menu-item', array('url' => '/', 'label' => 'Voir le site', 'target' => '_blank', 'rank' => 20)));
 	}
 
 	public function menu()
 	{
+		$this->menu->sort();
 		return $this->menu;
 	}
 
--- src/Aamant/Backoffice/BackofficeServiceProvider.php
+++ src/Aamant/Backoffice/BackofficeServiceProvider.php
@@ -39,7 +39,7 @@ class BackofficeServiceProvider extends ServiceProvider {
 	public function register()
 	{
 		$this->app->bindShared('backoffice', function() {
-			return new Backoffice(\App::make('menu'));
+			return new Backoffice(\App::make('menu', array('label' => 'Principal')));
 		});
 	}
 
